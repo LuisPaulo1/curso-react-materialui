b26da0063725f63338d5a6a46ffce9eb1ccddaaf
LuisPaulo1 <csluispaulo@gmail.com>
2024-05-17 15:46:44 -0300

    feat: Adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
   return (
-    <div className="App">
-      Teste
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <AppRoutes />
+      </div>
+    </BrowserRouter>
   );
 }
