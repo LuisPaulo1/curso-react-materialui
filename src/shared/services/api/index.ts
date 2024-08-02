767b131d00a15a5825bf9d35905f6ce34115cf86
LuisPaulo1 <csluispaulo@gmail.com>
2024-08-02 17:51:53 -0300

    Configuração Axios

--- src/shared/services/api/index.ts
+++ src/shared/services/api/index.ts
@@ -1,1 +1,13 @@
-export {};
+import axios from 'axios';
+import { errorInterceptor, responseInterceptor } from './axios-config/interceptors';
+
+const Api = axios.create({
+  baseURL: 'http://localhost:3333',
+});
+
+Api.interceptors.response.use(
+  (response) => responseInterceptor(response),
+  (error) => errorInterceptor(error),
+);
+
+export { Api };
