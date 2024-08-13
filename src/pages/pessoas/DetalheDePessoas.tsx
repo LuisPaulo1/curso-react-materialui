8b5fdc5ec0b391ebc5f005294db1d4806e17b8e4
LuisPaulo1 <csluispaulo@gmail.com>
2024-08-12 22:29:05 -0300

    Consulta de registro no backend com React

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,23 +1,68 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, LinearProgress } from "@mui/material";
 import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { FerramentasDeDetalhe } from "../../shared/components";
+import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 
 export const DetalheDePessoas: React.FC = () => {
   const { id = 'nova' } = useParams<'id'>();
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [nome, setNome] = useState('');
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [selectedId, setSelectedId] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (id !== 'nova') {
+      setIsLoading(true);
+      PessoasService.getById(Number(id))
+        .then(result => {
+          setIsLoading(false);
+          if (result instanceof Error) {
+            alert(result.message);
+            navigate('/pessoas');
+          } else {
+            setNome(result.nomeCompleto);
+            console.log(result);
+          }
+        });
+    }
+  }, []);
 
   const handleSave = () => {
     alert('Salvar');
   }
 
-  const handleDelete = () => {
-    alert('Apagar');
-  }
+  const handleOpenDialog = (id: number) => {
+    setSelectedId(id);
+    setIsDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setIsDialogOpen(false);
+    setSelectedId(null);
+  };
+
+  const handleConfirmDelete = () => {
+    if (selectedId !== null) {
+      PessoasService.deleteById(selectedId)
+        .then(result => {
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            alert('Registro apagado com sucesso!');
+            navigate('/pessoas');
+          }
+        });
+    }
+    handleCloseDialog();
+  };
 
   return (
     <LayoutBaseDePagina
-      titulo="Detalhe de Pessoas"
+      titulo={id === 'nova' ? 'Nova pessoa' : nome}
       barraDeFerramentas={
         <FerramentasDeDetalhe
           textoBotaoNovo='Nova'
@@ -27,13 +72,38 @@ export const DetalheDePessoas: React.FC = () => {
 
           aoClicarEmSalvar={handleSave}
           aoClicarEmSalvarEFechar={handleSave}
-          aoClicarEmApagar={handleDelete}
+          aoClicarEmApagar={() => handleOpenDialog(Number(id))}
           aoClicarEmNovo={() => navigate('/pessoas/detalhe/nova')}
           aoClicarEmVoltar={() => navigate('/pessoas')}
         />
       }
     >
+
+      {isLoading && (
+        <LinearProgress variant="indeterminate" />
+      )}
+
       <p>Detalhe de Pessoas {id}</p>
+
+      <Dialog
+        open={isDialogOpen}
+        onClose={handleCloseDialog}
+      >
+        <DialogTitle>Confirmação</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Realmente deseja apagar?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleConfirmDelete} color="primary" autoFocus>
+            Confirmar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </LayoutBaseDePagina>
   )
 }
